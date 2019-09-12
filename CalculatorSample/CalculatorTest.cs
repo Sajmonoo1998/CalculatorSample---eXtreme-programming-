3974e3e8ff90d3de08e877fb015947700dbbfd0b
Szymon Soczewka <sz.soczewka@gmail.com>
2019-09-12 11:06:16 +0200

    Initial commit

---
+++ CalculatorSample/CalculatorTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CalculatorSample
+{
+    public class CalculatorTest
+    {
+        [Theory]
+        [InlineData(0,int.MaxValue,int.MinValue)]
+        [InlineData(0, int.MinValue, int.MaxValue)]
+        [InlineData(5,0,5)]
+        public void Add_Valid_Input(int initial,int x,int expected)
+        {
+            ICalculator c = new Calculator();
+            c.Add(initial);
+            c.Add(x);
+            Assert.Equal(x, c.Result);
+        }
+
+        [Fact]
+        public void Add_Overflow_Expect_OverflowException()
+        {
+        // Arrange
+        ICalculator c = new Calculator();
+        c.Add(1);
+            // Act + Assert
+        var e = Assert.Throws<OverflowException>(() =>
+            {
+                c.Add(int.MaxValue);
+            });
+
+            Assert.True(e.Message == "Overflow by addition");
+        }
+
+
+        public void Add_Underflow_Expect_OverflowException()
+        {
+            // Arrange
+            ICalculator c = new Calculator();
+            c.Add(1);
+            // Act + Assert
+            var e = Assert.Throws<OverflowException>(() =>
+            {
+                c.Add(int.MinValue);
+            });
+
+            Assert.True(e.Message == "Overflow by addition");
+        }
+
+    }
+}
