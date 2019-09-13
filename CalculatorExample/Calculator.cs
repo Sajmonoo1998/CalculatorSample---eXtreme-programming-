1e32e5616506c5cd6f26894cf85036438ddb171b
Szymon Soczewka <sz.soczewka@gmail.com>
2019-09-13 17:18:19 +0200

    Implementation

--- CalculatorExample/Calculator.cs
+++ CalculatorExample/Calculator.cs
@@ -9,36 +9,54 @@ namespace CalculatorExample
         public int Result { get; private set; }
         public void Add(int x) // 1 + MaxValue < MaxValue
         {
-            if(x > 0 && (Result + x) <Result )
+            if(x>0 && (Result + x)<Result )
                 throw new OverflowException("Overflow by addition.");
-            if(x<0 && (Result + x)> Result)
+            if(x<0 && (Result + x)>Result)
                 throw new OverflowException("Underflow by addition.");
             Result += x;
         }
 
         public void Divide(int x)
         {
-            throw new System.NotImplementedException();
+            if (x == 0)
+                throw new DivideByZeroException("You can't divide by 0.");
+            Result /= x;
         }
 
-        public void Modulus(int x)
+        public void Modulus(int x) // reszta z 
         {
-            throw new System.NotImplementedException();
+            if (x == 0)
+                throw new DivideByZeroException("You can't mod by 0.");
+            Result = Result % x;
         }
 
         public void Multiply(int x)
         {
-            throw new System.NotImplementedException();
+            if(Result > 0 && x > 0 && (x * Result) <0)
+                throw new OverflowException("Underflow by multiplying.");
+            if(Result < 0 && x < 0 && (x * Result) <0)
+                throw new OverflowException("Underflow by multiplying.");
+            if(Result > 0 && x < 0 && (x * Result) >0)
+                throw new OverflowException("Overflow by multiplying.");
+            if(Result < 0 && x > 0 && (x * Result) >0)
+                throw new OverflowException("Overflow by multiplying.");
+            Result *= x;
         }
 
         public void Reset()
         {
-            throw new System.NotImplementedException();
+            Result = 0;
         }
 
         public void Subtract(int x)
         {
-            throw new System.NotImplementedException();
+            if(Result<0 && x>0 && (Result-x) >0 )
+                throw new OverflowException("Overflow by subtraction.");
+            if (Result > 0 && x < 0 && (Result - x) < 0)
+                throw new OverflowException("Underflow by subtraction.");
+
+            Result -= x;
+
         }
     }
 }
--- XUnitTestProject1/CalculatorTest.cs
+++ XUnitTestProject1/CalculatorTest.cs
@@ -9,10 +9,15 @@ namespace CalculatorXUnitTestProject1
 {
     public class CalculatorTest
     {
+
+
+        // Adding
         [Theory]
-        [InlineData(0,int.MinValue, int.MaxValue)]
-        [InlineData(0, int.MinValue, int.MaxValue)]
-        [InlineData(5,0,5)]
+        [InlineData(0, int.MinValue, int.MinValue)] 
+        [InlineData(0, int.MaxValue, int.MaxValue)] 
+        [InlineData(1_000_000, 1_000_000, 2_000_000)] 
+        [InlineData(5, 0, 5)]
+        [InlineData(123,123,246)]
         public void Add_Valid_Input(int initial,int x,int expected)
         {
             ICalculator c = new Calculator();
@@ -27,13 +32,13 @@ namespace CalculatorXUnitTestProject1
         // Arrange
         ICalculator c = new Calculator();
         c.Add(1);
-            // Act + Assert
+        // Act + Assert
         var e = Assert.Throws<OverflowException>(() =>
             {
                 c.Add(int.MaxValue);
             });
 
-            Assert.True(e.Message == "Overflow by addition");
+            Assert.True(e.Message == "Overflow by addition.");
         }
 
 
@@ -42,15 +47,195 @@ namespace CalculatorXUnitTestProject1
         {
             // Arrange
             ICalculator c = new Calculator();
-            c.Add(1);
+            c.Add(-1);
             // Act + Assert
             var e = Assert.Throws<OverflowException>(() =>
             {
                 c.Add(int.MinValue);
             });
 
-            Assert.True(e.Message == "Overflow by addition");
+            Assert.True(e.Message == "Underflow by addition.");
+        }
+        /////////
+        //Subtracting
+        [Theory]
+        [InlineData(int.MaxValue, 1, int.MaxValue-1)]
+        [InlineData(5000, 3500, 1500)]
+        [InlineData(-1_000_000, 1_000_000, -2_000_000)]
+        [InlineData(-5, 4, -9)]
+        [InlineData(246, -123, 369)]
+        public void Subtract_Valid_Input(int initial, int x, int expected)
+        {
+            ICalculator c = new Calculator();
+            c.Add(initial);
+            c.Subtract(x);
+            Assert.Equal(expected, c.Result);
         }
 
+        [Fact]
+        public void Subtract_Overflow_Expect_OverflowException()
+        {
+            // Arrange
+            ICalculator c = new Calculator();
+            c.Add(int.MinValue);  
+            // Act + Assert
+            var e = Assert.Throws<OverflowException>(() =>
+            {                               
+                c.Subtract(1);  
+                
+            });
+
+            Assert.True(e.Message == "Overflow by subtraction.");
+        }
+
+
+        [Fact]
+        public void Subtract_Underflow_Expect_OverflowException()
+        {
+            // Arrange
+            ICalculator c = new Calculator();
+            c.Add(1); 
+            // Act + Assert
+            var e = Assert.Throws<OverflowException>(() =>
+            {
+                c.Subtract(-int.MaxValue); 
+            });
+
+            Assert.True(e.Message == "Underflow by subtraction.");
+        }
+        /////////////
+        ///Divide
+        [Theory]
+        [InlineData(10000, 4, 2500)]
+        [InlineData(5000, 2, 2500)]
+        [InlineData(-1_000_000, 1000, -1000)]
+        [InlineData(10, 5, 2)]
+        [InlineData(333, 3, 111)]
+        public void Divide_Valid_Input(int initial, int x, int expected)
+        {
+            ICalculator c = new Calculator();
+            c.Add(initial);
+            c.Divide(x);
+            Assert.Equal(expected, c.Result);
+        }
+
+        [Fact]
+        public void Divide_Expect_DivideByZeroException()
+        {
+            // Arrange
+            ICalculator c = new Calculator();
+            c.Add(int.MinValue);
+            // Act + Assert
+            var e = Assert.Throws<DivideByZeroException>(() =>
+            {                              
+                c.Divide(0);  
+
+            });
+
+            Assert.True(e.Message == "You can't divide by 0.");
+        }
+        //////
+        ///MULTIPLY
+        [Theory]
+        [InlineData(10000, 4, 40000)]
+        [InlineData(5000, 2, 10000)]
+        [InlineData(-1_000_000, 10, -10_000_000)]
+        [InlineData(123_456, 12, 1_481_472)]
+        [InlineData(333333, 333, 110_999_889)]
+        public void Multiply_Valid_Input(int initial, int x, int expected)
+        {
+            ICalculator c = new Calculator();
+            c.Add(initial);
+            c.Multiply(x);
+            Assert.Equal(expected, c.Result);
+        }
+
+        [Fact]
+        public void Multiply_Overflow_Expect_OverflowException()
+        {
+            // Arrange
+            ICalculator c = new Calculator();
+            c.Add(int.MinValue+10000);
+            // Act + Assert
+            var e = Assert.Throws<OverflowException>(() =>
+            {
+                c.Multiply(2);
+
+            });
+            c.Reset();
+            c.Add(2);
+            // Act + Assert
+            var e2 = Assert.Throws<OverflowException>(() =>
+            {
+                c.Multiply(int.MinValue + 15000);
+
+            });
+
+            Assert.True(e.Message == "Overflow by multiplying." && e2.Message == "Overflow by multiplying.");
+        }
+        [Fact]
+        public void Multiply_Underflow_Expect_OverflowException()
+        {
+            // Arrange
+            ICalculator c = new Calculator();
+            c.Add(int.MaxValue-10000);
+            // Act + Assert
+            var e = Assert.Throws<OverflowException>(() =>
+            {
+                c.Multiply(2);
+
+            });
+            c.Reset();
+            c.Add(2);
+            // Act + Assert
+            var e2 = Assert.Throws<OverflowException>(() =>
+            {
+                c.Multiply(int.MaxValue - 15000);
+
+            });
+
+            Assert.True(e.Message == "Underflow by multiplying." && e.Message == "Underflow by multiplying.");
+        }
+        //////
+        ///Modulus
+        [Theory]
+        [InlineData(101, 100, 1)]
+        [InlineData(123, 20, 3)]
+        [InlineData(125, 25, 0)]
+        [InlineData(10, 4, 2)]
+        [InlineData(57, 5, 2)]
+        public void Modulo_Valid_Input(int initial, int x, int expected)
+        {
+            ICalculator c = new Calculator();
+            c.Add(initial);
+            c.Modulus(x);
+            Assert.Equal(expected, c.Result);
+        }
+
+        [Fact]
+        public void Modulo_Expect_DivideByZeroException()
+        {
+            // Arrange
+            ICalculator c = new Calculator();
+            c.Add(int.MinValue);
+            // Act + Assert
+            var e = Assert.Throws<DivideByZeroException>(() =>
+            {
+                c.Modulus(0);
+
+            });
+
+            Assert.True(e.Message == "You can't mod by 0.");
+        }
+
+        [Fact]
+        public void Reset_Expect_Zero()
+        {
+            ICalculator c = new Calculator();
+            var expected = 0;
+            c.Add(123456);
+            c.Reset();
+            Assert.Equal(expected, c.Result);
+        }
     }
 }
